40a576c8aaa6d4c83fc4871a0da297510dab5d87
Ashok <ashokansoft@gmail.com>
2023-02-15 17:46:50 +0530

    Form Created -dude

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass:HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ portfolio/src/app/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+  
+  FName:any
+  LName:any
+  age:any
+  gender:any
+  department:any
+  email:any
+  phoneNumber:any
+  password:any
+  ConfrimPass:any
+
+}
