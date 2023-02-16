28589370ca83ec5ead98ef7c8342f468dc8abdd5
Ashok <ashokansoft@gmail.com>
2023-02-16 10:50:32 +0530

    check box have some issue just safty commit

--- portfolio/src/app/form/form.component.ts
+++ portfolio/src/app/form/form.component.ts
@@ -13,9 +13,41 @@ export class FormComponent {
   age:any
   gender:any
   department:any
+  work:any
   email:any
   phoneNumber:any
   password:any
   ConfrimPass:any
+  successresult:any
+
+  submit(){
+
+    if(this.FName,this.LName,this.age,this.gender,this.department,this.email,
+      this.phoneNumber,this.password,this.ConfrimPass!=null){
+        
+        console.log("Form Submitted")
+        this.successresult="submitted successfully"
+
+        console.log("FirstNAME:"+this.FName)
+        console.log("LastNAME:"+this.LName)
+        console.log("Age:"+this.age)
+        console.log("Gender :"+this.gender)
+        console.log("Depart :"+this.department)
+        console.log("Working locations user wish:"+this.work)
+
+        console.log("PH :"+this.phoneNumber)
+        console.log("Pass : "+this.password)
+        console.log("Cpass :"+this.ConfrimPass)
+        // console.log(""+this.successresult)
+        alert("User Datas"+this.FName+ "Last Name : "+this.LName+"Age : "+this.age+"gender : "+this.gender+"department :"+
+        this.department+"Work"+this.work+"PhoneNumber : "+this.phoneNumber+"Password :"+this.password+"CPass"+this.ConfrimPass)
+       
+        
+
+  }
+  else{
+    alert("hoo!! Sorry!!! Some Fields Are Empty")
+  }
+}
 
 }
