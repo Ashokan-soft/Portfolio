1a988e398f339e57811b162e84e55599267cda9d
Ashok <ashokansoft@gmail.com>
2023-02-16 23:58:28 +0530

    successfully nav created

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component: FormComponent, pathMatch: 'full' },
-  {path:'form',component:FormComponent}
+  // { path: '', component: FormComponent, pathMatch: 'full' },
+  {path:'form',component:FormComponent},
+  {path:'profile',component:ProfileComponent}
+
   
 ];
 
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,19 +6,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule,
+  
   ],
   providers: [
-    { provide: LocationStrategy, useClass:HashLocationStrategy}
+    // { provide: LocationStrategy, useClass:HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
