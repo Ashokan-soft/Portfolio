fb23d595bbb72033455a3204639c79ae913c0db8
Ashok <ashokansoft@gmail.com>
2023-02-16 11:47:52 +0530

    password  submiit still have checkbox issue

--- portfolio/src/app/form/form.component.ts
+++ portfolio/src/app/form/form.component.ts
@@ -11,6 +11,7 @@ export class FormComponent {
   FName:any
   LName:any
   age:any
+  dob:any
   gender:any
   department:any
   work:any
@@ -19,29 +20,54 @@ export class FormComponent {
   password:any
   ConfrimPass:any
   successresult:any
+  tempdate:any="10-10-2023"
 
   submit(){
 
     if(this.FName,this.LName,this.age,this.gender,this.department,this.email,
-      this.phoneNumber,this.password,this.ConfrimPass!=null){
-        
-        console.log("Form Submitted")
-        this.successresult="submitted successfully"
-
-        console.log("FirstNAME:"+this.FName)
-        console.log("LastNAME:"+this.LName)
-        console.log("Age:"+this.age)
-        console.log("Gender :"+this.gender)
-        console.log("Depart :"+this.department)
-        console.log("Working locations user wish:"+this.work)
-
-        console.log("PH :"+this.phoneNumber)
-        console.log("Pass : "+this.password)
-        console.log("Cpass :"+this.ConfrimPass)
-        // console.log(""+this.successresult)
-        alert("User Datas"+this.FName+ "Last Name : "+this.LName+"Age : "+this.age+"gender : "+this.gender+"department :"+
-        this.department+"Work"+this.work+"PhoneNumber : "+this.phoneNumber+"Password :"+this.password+"CPass"+this.ConfrimPass)
+      this.phoneNumber,this.password,this.ConfrimPass,this.dob!=null){
        
+        
+        // if(this.dob-this.tempdate!=0,[-1-0])
+        // {
+        //   console.log("Error in date")
+        // }
+        // else{
+        //   console.log("date is ok")
+        // }
+
+        if(this.password != this.ConfrimPass){
+
+          console.log("Password Has a Problem")
+          alert("Check Password")
+
+        }
+        else{
+
+          console.log("Form Submitted")
+          this.successresult="submitted successfully"
+  
+          console.log("FirstNAME:"+this.FName)
+          console.log("LastNAME:"+this.LName)
+          console.log("Age:"+this.age)
+          console.log("Gender :"+this.gender)
+          console.log("Depart :"+this.department)
+          console.log("Working locations user wish:"+this.work)
+  
+          console.log("PH :"+this.phoneNumber)
+          console.log("Pass : "+this.password)
+          console.log("Cpass :"+this.ConfrimPass)
+          // console.log(""+this.successresult)
+
+          alert("User Datas ::"+"FirstName"+this.FName+ "Last Name : "+this.LName+"Age : "+this.age+"gender : "+this.gender+"department :"+
+          this.department+"Work"+this.work+"PhoneNumber : "+this.phoneNumber+"Password :"+this.password+"CPass"+this.ConfrimPass)
+         
+
+        }
+
+
+
+
         
 
   }
