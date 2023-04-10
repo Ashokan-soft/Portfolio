6f3ac97ecec132f7cede917421e913bc9e782a1f
Ashok <ashokansoft@gmail.com>
2023-04-10 08:25:01 +0530

    flow check eduction

--- portfolio/src/app/about/about.component.ts
+++ portfolio/src/app/about/about.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ScriptCheckService } from '../services/script-check.service';
+
+const SCRIPT_PATH = '/assets/About.js';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  
+    
+  constructor(
+    private renderer: Renderer2,
+    private scriptService: ScriptCheckService,
+  ) { }
+
+  ngOnInit(): void {const scriptElement = this.scriptService.loadJsScript(this.renderer, SCRIPT_PATH); 
+    
+   
+
+  }
 }
